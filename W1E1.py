2f0e39d0b78fa07ab1a13c7510ecb425e8b2fa91
kutnatsen <andersenemil12@gmail.com>
2021-12-21 10:01:45 +0100

    W1E1+E2 done

---
+++ W1E1.py
@@ -0,0 +1,14 @@
+'''
+Create a Python script that has three variables:
+ip_addr1, ip_addr2, ip_addr3 (representing three corresponding IP addresses). 
+Print these three variables to standard output using a single print statement.
+Make your print statement compatible with both Python2 and Python3.
+'''
+
+from __future__ import print_function
+
+ip_addr1 = "1.1.1.1"
+ip_addr2 = "8.8.8.8"
+ip_addr3 = "192.168.1.1"
+
+print(ip_addr1, ip_addr2, ip_addr3)
