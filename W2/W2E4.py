a303bc8d53b8b1526713018985e88970794683c0
kutnatsen <andersenemil12@gmail.com>
2021-12-21 14:02:23 +0100

    W2E4 Done

--- W2/W2E4.py
+++ W2/W2E4.py
@@ -1,10 +1,25 @@
 '''
-Read in the "show_ip_int_brief.txt" file into your program using the .readlines() method.
-Obtain the list entry associated with the FastEthernet4 interface. 
-You can just hard-code the index at this point since we haven't covered for-loops or regular expressions.
-Use the string .split() method to obtain both the IP address and the corresponding interface associated with the IP.
+Read in the "show_ip_int_brief.txt" file into
+your program using the .readlines() method.
+Obtain the list entry associated with the FastEthernet4 interface.
+You can just hard-code the index at this
+point since we haven't covered for-loops or regular expressions.
+Use the string .split() method to obtain
+both the IP address and the corresponding interface associated with the IP.
 Create a two element tuple from the result (intf_name, ip_address).
 Print that tuple to the screen.
-Use pycodestyle on this script. Get the warnings/errors to zero. 
-You might need to 'pip install pycodestyle' on your computer (you should be able to type this from the shell prompt). Alternatively, you can type 'python -m pip install pycodestyle'.
+Use pycodestyle on this script. Get the warnings/errors to zero.
+You might need to 'pip install pycodestyle' on your computer
+(you should be able to type this from the shell prompt). Alternatively,
+you can type 'python -m pip install pycodestyle'.
 '''
+with open("show_ip_int_brief.txt") as f:
+    output = f.readlines()
+
+fa4_ip = output[5].strip()
+fields = fa4_ip.split()
+interface = fields[0]
+ip = fields[1]
+
+my_results = (interface, ip)
+print(my_results)
