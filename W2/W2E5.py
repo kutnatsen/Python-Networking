f48e6384861c2f81bed22b035f1a4c5e379d0226
kutnatsen <andersenemil12@gmail.com>
2021-12-21 14:12:43 +0100

    W2E5 Done

--- W2/W2E5.py
+++ W2/W2E5.py
@@ -5,3 +5,17 @@ From the first line use the string .split() method to obtain the local AS number
 From the last line use the string .split() method to obtain the BGP peer IP address.
 Print both local AS number and the BGP peer IP address to the screen.
 '''
+
+with open("show_ip_bgp_summ.txt") as f:
+    output = f.read()
+
+output = output.splitlines()
+
+first_line = output[0]
+as_num = first_line.split()[-1]
+
+last_line = output[-1]
+bgp_peer = last_line.split()[0]
+
+print("Local AS: {}".format(as_num))
+print("BGP Peer ip address: {}".format(bgp_peer))
