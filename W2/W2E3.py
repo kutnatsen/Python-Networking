93be66b0a216db4160e7f472d448a0e3cc3da1e1
kutnatsen <andersenemil12@gmail.com>
2021-12-21 13:41:15 +0100

    W2E3 Done

--- W2/W2E3.py
+++ W2/W2E3.py
@@ -8,3 +8,18 @@ Create a new list slice that is only the first three ARP entries.
 Use the .join() method to join these first three ARP entries back together as a single string using the newline character ('\n') as the separator.
 Write this string containing the three ARP entries out to a file named "arp_entries.txt".
 '''
+from pprint import pprint
+
+with open("show_arp.txt") as f:
+    output = f.readlines()
+
+output = output[1:]
+pprint(output)
+
+output.sort()
+
+new_output = output[:3]
+new_output = "\n".join(new_output)
+
+with open("arp_entries.txt", "wt") as w:
+    w.write(new_output)
