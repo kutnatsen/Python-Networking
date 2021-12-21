1edc81cf88b5d2941bfaddcad7e50f78930b7cb9
kutnatsen <andersenemil12@gmail.com>
2021-12-21 13:15:56 +0100

    W2E1 Done

--- W2/W2E1.py
+++ W2/W2E1.py
@@ -7,3 +7,22 @@ Open the file a second time using a Python context manager (with statement).
 Read in the file contents using the .readlines() method. Print out the file contents to the screen. 
 Also print out the type of the variable (you should have a list at this point).
 '''
+banner = "-" * 80
+
+f = open("show_version.txt")
+output = f.read()
+print(banner)
+print(output)
+print(banner)
+print(type(output))
+print(banner)
+f.close()
+
+with open("show_version.txt") as f:
+    output = f.readlines()
+
+print(banner)
+print(output)
+print(banner)
+print(type(output))
+print(banner)
