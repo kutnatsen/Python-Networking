14663b217d0512bf7d5fc581a51d7bb4b547efd4
kutnatsen <andersenemil12@gmail.com>
2021-12-21 13:29:24 +0100

    W2E2 Done

--- W2/W2E2.py
+++ W2/W2E2.py
@@ -10,3 +10,19 @@ Using the .pop() method to remove the first IP address in the list and the last 
 Update the new first IP address in the list to be '2.2.2.2'. 
 Print out the new first IP address in the list.
 '''
+
+
+ip_list = ["1.1.1.1","10.10.10.10","100.100.100.100","50.50.50.50","30.30.30.30"]
+ip_list.append("40.40.40.40")
+ip_list.extend(["60.60.60.60","70.70.70.70"])
+ip_list = ip_list + ["80.80.80.80", "90.90.90.90"]
+print(ip_list)
+
+print(ip_list[0])
+print(ip_list[-1])
+
+first_ip = ip_list.pop(0)
+last_ip = ip_list.pop(-1)
+
+ip_list[0] = "2.2.2.2"
+print(ip_list[0])
