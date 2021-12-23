f04c7a14bb13e13a672ca1c5e3362a98767856df
kutnatsen <andersenemil12@gmail.com>
2021-12-23 09:42:39 +0100

    W3E2 Done

--- W3/W3E2.py
+++ W3/W3E2.py
@@ -8,3 +8,26 @@ If this IP address is found, then print out "Arista3 IP/Mac is" and the correspo
 Keep track of whether you have found both the Default Gateway and the Arista3 switch. 
 Once you have found both of these devices, 'break' out of the for loop.
 '''
+
+with open("show_arp.txt") as f:
+    output = f.read()
+
+ip_addr_list = []
+mac_addr_list = []
+x = 0
+for line in output.splitlines():
+    if "Protocol" in line:
+        continue
+    fields = line.split()
+    ip_addr = fields[1]
+    mac_addr = fields[3]
+    if ip_addr ==  "10.220.88.1":
+        print("Default gateway IP/Mac: {}/{}".format(ip_addr, mac_addr))
+        x += 1
+    elif ip_addr == "10.220.88.30":
+        print("Arista3 IP/Mac is: {}/{}".format(ip_addr, mac_addr))
+        x += 1
+    if x == 2:
+        break
+    
+
