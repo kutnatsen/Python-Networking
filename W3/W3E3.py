0d1102a41a2a8b40b9a4ec32e26bafbd9a17aced
kutnatsen <andersenemil12@gmail.com>
2021-12-23 09:49:17 +0100

    W3E3 Done

--- W3/W3E3.py
+++ W3/W3E3.py
@@ -6,3 +6,22 @@ Save these two items into variables and print them to the screen.
 You should extract only the system name and port id from the lines (i.e. your variables should only have 'twb-sf-hpsw1' and '15'). 
 Break out of your loop once you have retrieved these two items.
 '''
+
+with open("show_lldp_neighbors_detail.txt") as f:
+    output = f.read()
+
+x = 0
+
+for line in output.splitlines():
+    if "Port id" in line:
+        port_id = line.split()[2]
+        x += 1
+    elif "System Name" in line:
+        system_name = line.split()[2]
+        x += 1
+    
+    if x == 2:
+        break
+print(system_name, port_id)
+        
+    
