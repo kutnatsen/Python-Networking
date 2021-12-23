6a56857bce4644799caf718e1c43ef406775a089
kutnatsen <andersenemil12@gmail.com>
2021-12-23 11:00:29 +0100

    added pings with hostname

--- W3/W3Extra2.py
+++ W3/W3Extra2.py
@@ -4,15 +4,23 @@ with open("iplist.csv") as f:
     output = f.read()
 
 my_ip_addr_list = []
+my_hostname_list = []
+
 for line in output.splitlines():
     if "hostname" in line:
         continue
     fields = line.split(",")
     ip = fields[1]
     my_ip_addr_list.append(ip)
+    hostname = fields[0]
+    my_hostname_list.append(hostname)
 
 for ip in my_ip_addr_list:
     print("-" * 60)
     ping = "ping -n 2 " + ip
     os.system(ping)  
 
+for hostname in my_hostname_list:
+    print("-" * 60)
+    ping = "ping -n 2 " + hostname
+    os.system(ping)
